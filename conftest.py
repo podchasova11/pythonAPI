2fe9e50b4cadcce709c444fbb2657ce4ea5541c1
Mila Podchasova <Pod4asova20@gmail.com>
2023-12-25 12:47:24 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,14 +3,14 @@ import pytest
 import requests
 
 
-@pytest.fixture()
-def connect_data_base():
-    connection = sqlite3.connect("test.db")
-    print("Мы подключили БД")
-    return connection
+# @pytest.fixture()
+# def connect_data_base():
+#     connection = sqlite3.connect("test.db")
+#     print("Мы подключили БД")
+#     return connection
 
 
-cursor = connect_data_base().cursor()
-cursor.execute("SELECT *FROM employees")
-cursor.fetchall()  # собери все записи и запринть
-connect_data_base().close()   #закрывает БД
+# cursor = connect_data_base().cursor()
+# cursor.execute("SELECT *FROM employees")
+# cursor.fetchall()  # собери все записи и запринть
+# connect_data_base().close()   #закрывает БД
