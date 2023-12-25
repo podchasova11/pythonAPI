b0f732287c58426cae83ba344291681584ff77c8
podchasova11 <Pod4asova20@gmail.com>
2023-12-25 12:38:17 +0300

    added conftest

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import sqlite3
+import pytest
+import requests
+
+
+@pytest.fixture()
+def connect_data_base():
+    connection = sqlite3.connect("test.db")
+    print("Мы подключили БД")
+    return connection
+
+
+cursor = connect_data_base().cursor()
+cursor.execute("SELECT *FROM employees")
+cursor.fetchall()  # собери все записи и запринть
+connect_data_base().close()   #закрывает БД
