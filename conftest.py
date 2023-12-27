9c6b9a78e16366bd43a9ba52f69ee8677d76b4f5
podchasova11 <Pod4asova20@gmail.com>
2023-12-27 23:49:08 +0300

    added new

--- conftest.py
+++ conftest.py
@@ -1,16 +1,32 @@
 import sqlite3
 import pytest
-import requests
+import time
 
 
-# @pytest.fixture()
-# def connect_data_base():
-#     connection = sqlite3.connect("test.db")
-#     print("Мы подключили БД")
-#     return connection
+@pytest.fixture()
+def connect_data_base():
+    connection = sqlite3.connect("test.db")
+    print("Мы подключили БД")
+    return connection
 
 
-# cursor = connect_data_base().cursor()
-# cursor.execute("SELECT *FROM employees")
-# cursor.fetchall()  # собери все записи и запринть
-# connect_data_base().close()   #закрывает БД
+cursor = connect_data_base().cursor()
+cursor.execute("SELECT *FROM employees")
+cursor.fetchall()  # собери все записи и запринть
+connect_data_base().close()   #закрывает БД
+
+
+@pytest.fixture()
+def connect_data_base(self):
+    connection = sqlite3.connect("test.db")
+    print("Мы подключили БД")
+    return connection
+
+
+@pytest.fixture()
+def generate_data_way_2(request):
+
+    login = f"autotest_{time.time()}@ya.ru"
+    password = "123"
+    request.cls.login = login
+    request.cls.password = password
